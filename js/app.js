8df97fa10fc7fa362ca1b59450bbb7e9d1e61ba7
RohanPNaher <RohanPNaher@gmail.com>
2022-02-01 16:52:16 -0500

    solve 04

--- js/app.js
+++ js/app.js
@@ -21,7 +21,7 @@ let timerId = setTimeout(writeDing, 3000);
 // Write the sorting callback as a named function declaration
 let compareLength = function(wordA, wordB){
   return wordA.length - wordB.length
-}
+};
 // Write the callback function to provide to the sort method so that
 // the following code sorts the words from shortest to longest.
 
@@ -48,3 +48,32 @@ console.log(words)
 
 // Check that logging longerWords outputs
 // ["lengthy", "delicious"]
+
+
+
+// Exercise 4
+
+// Code a forEach method:
+// 		1. Write a function named forEach.
+//		2. The forEach function accepts two args, an array & a callback.
+//		3. Code the forEach method to iterate over each element in the array arg (use a for loop).
+//		4. For each iteration, invoke the callback arg, passing to it the element and the index of the element.
+
+// Test with this array
+const colors = ['red', 'green', 'blue', 'purple'];
+// and this callback
+function log(elem, idx) {
+  console.log(`Index: ${idx} / Element Value: ${elem}`);
+}
+
+let forEach = (arg, callback) => {
+  for (let i = 0; i < arg.length; i++) {
+    callback(arg[i], i)
+  }
+};
+console.log(forEach(colors, log))
+// calling forEach(colors, log) should resulting in this output:
+// Index: 0 / Element Value: red
+// Index: 1 / Element Value: green
+// Index: 2 / Element Value: blue
+// Index: 3 / Element Value: purple
