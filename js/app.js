8db1a00bcfe9ee787f4f1a92e5112de9f46798a7
RohanPNaher <RohanPNaher@gmail.com>
2022-02-01 16:26:42 -0500

    solve 02

--- js/app.js
+++ js/app.js
@@ -4,9 +4,31 @@
 // wait three seconds, then write "Ding!" to the console.  Instead, it writes
 // "Ding!" immediately.  Find the bug and fix it.
 
-let ringer = function writeDing() {
+let writeDing = function() {
   console.log('Ding!');
 }
 
-let timerId = setTimeout(ringer, 3000);
+let timerId = setTimeout(writeDing, 3000);
 
+
+// Exercise 2
+
+// Javascript arrays have a built-in sort method that can take
+// a callback to tell it how to compare the things you want to sort.
+
+// Research the array sort method.
+
+// Write the sorting callback as a named function declaration
+let compareLength = function(wordA, wordB){
+  return wordA.length - wordB.length
+}
+// Write the callback function to provide to the sort method so that
+// the following code sorts the words from shortest to longest.
+
+const words = ['short', 'medium', 'delicious', 'nice', 'lengthy'];
+
+// The sort method sorts "in place", that is, it modifies the array
+let wordsSort = words.sort(compareLength);
+console.log(wordsSort)
+// Check that logging words now outputs
+// ["nice", "short", "medium", "lengthy", "delicious"]
